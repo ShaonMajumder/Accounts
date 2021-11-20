b14886b3250eb71686a56fe107a231372b8e7dd6
Shaon Majumder <smazoomder@gmail.com>
2021-11-20 18:43:38 +0600

    big buttons pagination fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,13 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function users_index()
+    {
+        $users =  User::paginate(5);
+        return view('users', [
+            'users' => $users
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a href="{{ route('users') }}">Users</a>
                 </div>
             </div>
         </div>
