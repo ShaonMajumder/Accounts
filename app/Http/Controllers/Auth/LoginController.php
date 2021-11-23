317f532b382ea318898e6467ee139a680e845d9a
Shaon Majumder <smazoomder@gmail.com>
2021-11-23 16:47:37 +0600

    loggedout multi-site

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,16 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function authenticated(Request $request, $user) {
+        $back_url = session('back_url');
+        session(['back_url' => '']);
+        return redirect($back_url);
+    }
+    
+    function loggedOut(Request $request)
+    {
+        return redirect()->back();
     }
 }
